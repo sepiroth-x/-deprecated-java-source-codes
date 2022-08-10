1711633a4575e293d7b5a5252c35bad7734e850d
sepiroth-x <chardy.tsadiq@gmail.com>
2022-08-10 16:18:45 +0800

    RegistrationFormTutorial

---
+++ RegistrationForm/src/com/sepirothx/project/RegistrationForm.java
@@ -0,0 +1,20 @@
+package com.sepirothx.project;
+
+import javax.swing.*;
+
+public class RegistrationForm {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton btnCancel;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JButton btnRegister;
+    private JLabel labelFN;
+    private JLabel labelLN;
+    private JLabel labelEmail;
+    private JLabel labelPassword;
+    private JLabel labelConfirmPassword;
+
+
+}
