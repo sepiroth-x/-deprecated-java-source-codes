ff0f3133e2d4c86e79877f6514a0e18589719166
sepiroth-x <chardy.tsadiq@gmail.com>
2022-08-15 10:39:18 +0800

    Calculator
    To be continued . . .

---
+++ Calculator/src/com/sepirothx/calculator/Calculator.java
@@ -0,0 +1,33 @@
+package com.sepirothx.calculator;
+
+import javax.swing.*;
+
+public class Calculator extends JFrame {
+    private JTextField textField1;
+    private JButton jbtn1;
+    private JButton jbtnmultiply;
+    private JButton jbtn3;
+    private JButton jbtn2;
+    private JButton jbtn4;
+    private JButton jbtn5;
+    private JButton jbtn6;
+    private JButton jbtndivision;
+    private JButton jbtn7;
+    private JButton jbtn8;
+    private JButton jbtn9;
+    private JButton jbtnaddition;
+    private JButton jbtnminus;
+    private JButton jbtnequals;
+    private JPanel calculatorPanel;
+
+    public Calculator(){
+
+        setContentPane(calculatorPanel);
+        setTitle("Calculator");
+        setSize("");
+
+
+    }
+
+
+}
