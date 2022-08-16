6312c8a92c9ab18f5295187328e1286d54e7eb7f
sepiroth-x <chardy.tsadiq@gmail.com>
2022-08-16 10:00:24 +0800

    Basic Arithmetic Operators
    Final as for the moment

--- Calculator/src/com/sepirothx/calculator/Calculator.java
+++ Calculator/src/com/sepirothx/calculator/Calculator.java
@@ -21,15 +21,17 @@ public class Calculator extends JFrame {
     private JButton jbtnminus;
     private JButton jbtnequals;
     private JPanel calculatorPanel;
+    private JButton jbtn0;
 
     int firstNumber, secondNumber;
-    float result;
+    int result;
+    char operatorSymbol;
 
     public Calculator(){
 
         setContentPane(calculatorPanel);
         setTitle("Calculator");
-        setSize(250,210);
+        setSize(250,240);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
@@ -59,8 +61,147 @@ public class Calculator extends JFrame {
         jbtn3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String resultString = tfResult.getText(); //unsay data sa textfield ibutang niya sa variable
-                tfResult.setText(resultString + jbtn3.getText());//ibutang ang btn text (3) sa text field
+                String resultString = tfResult.getText();
+                tfResult.setText(resultString + jbtn3.getText());
+
+            }
+        });
+
+        jbtn4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String resultString = tfResult.getText();
+                tfResult.setText(resultString + jbtn4.getText());
+
+            }
+        });
+
+        jbtn5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String resultString = tfResult.getText();
+                tfResult.setText(resultString + jbtn5.getText());
+
+            }
+        });
+
+        jbtn6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String resultString = tfResult.getText();
+                tfResult.setText(resultString + jbtn6.getText());
+
+            }
+        });
+
+        jbtn7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String resultString = tfResult.getText();
+                tfResult.setText(resultString + jbtn7.getText());
+
+            }
+        });
+
+        jbtn8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String resultString = tfResult.getText();
+                tfResult.setText(resultString + jbtn8.getText());
+
+            }
+        });
+
+        jbtn9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String resultString = tfResult.getText();
+                tfResult.setText(resultString + jbtn9.getText());
+
+            }
+        });
+
+
+        jbtn0.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String resultString = tfResult.getText();
+                tfResult.setText(resultString + jbtn0.getText());
+
+            }
+        });
+
+
+        jbtnmultiply.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String textResult = tfResult.getText();
+                firstNumber = Integer.parseInt(textResult);
+                tfResult.setText(null);
+                operatorSymbol = 'x';
+
+            }
+        });
+        jbtnequals.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String textResult = tfResult.getText();
+                secondNumber = Integer.parseInt(textResult);
+                switch(operatorSymbol){
+                    case 'x':
+                        result = firstNumber * secondNumber;
+                        tfResult.setText(Integer.toString(result));
+                        break;
+                    case '+':
+                        result = firstNumber + secondNumber;
+                        tfResult.setText(Integer.toString(result));
+                        break;
+                    case '/':
+                        result = firstNumber / secondNumber;
+                        tfResult.setText(Float.toString(result));
+                        break;
+                    case '-':
+                        result = firstNumber - secondNumber;
+                        tfResult.setText(Integer.toString(result));
+                        break;
+                    default:
+                        tfResult.setText("Syntax Error");
+                        break;
+                }
+            }
+        });
+        jbtndivision.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String textResult = tfResult.getText();
+                firstNumber = Integer.parseInt(textResult);
+                tfResult.setText(null);
+                operatorSymbol = '/';
+
+            }
+        });
+
+
+        jbtnaddition.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String textResult = tfResult.getText();
+                firstNumber = Integer.parseInt(textResult);
+                tfResult.setText(null);
+                operatorSymbol = '+';
+            }
+        });
+
+
+        jbtnminus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String textResult = tfResult.getText();
+                firstNumber = Integer.parseInt(textResult);
+                tfResult.setText(null);
+                operatorSymbol = '-';
 
             }
         });
