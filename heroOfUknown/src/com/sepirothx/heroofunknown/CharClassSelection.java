33629a648fcc2ea0b1082b831032164f29d83c38
sepiroth-x <chardy.tsadiq@gmail.com>
2022-09-18 19:32:29 +0800

    Hero of Uknown (Attrib added)

--- heroOfUknown/src/com/sepirothx/heroofunknown/CharClassSelection.java
+++ heroOfUknown/src/com/sepirothx/heroofunknown/CharClassSelection.java
@@ -1,3 +1,13 @@
+/**
+ * Hero Of Unknown
+ * Category: Game / Text-based
+ * By: Sepiroth-X a.k.a Richard Cebel Cupal, LPT
+ * License: Open-Source (Feel free to copy and modify)
+ * The objective of this source code is to teach students
+ * the concepts in java language.
+ * **/
+
+
 package com.sepirothx.heroofunknown;
 
 public interface CharClassSelection {
